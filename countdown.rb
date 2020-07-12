60793a0fb3bc72b3e87f2586011fbf8c7a177030
Billy Gunn, Jr <bgunnjr@gmail.com>
2020-07-12 05:18:05 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,7 +8,7 @@ def countdown(number_of_seconds)
   "HAPPY NEW YEAR!"
 end
 
-def countdown(number_of_seconds)
+def countdown_with_sleep(number_of_seconds)
   while number_of_seconds > 0
     puts "#{number_of_seconds} SECOND(S)!"
     number_of_seconds -= 1 
